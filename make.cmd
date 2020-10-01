64accf91051af422fb3950a547879c1e4f67eebe
Robert Hilton <robert.a.hilton.jr@gmail.com>
2020-09-30 21:54:11 -0400

    Improve winmake

--- make.cmd
+++ make.cmd
@@ -5,4 +5,14 @@ for %%f in (*.proto) do (
     echo %%~nf
 	..\CKSimLibCpp\lib\protobuf\protoc.exe %%~nf.proto --cpp_out=generated
 )
+cd generated
+mkdir include
+mkdir src
+for %%f in (*.cc) do (
+	move %%~nf.cc src/ > nul
+)
+for %%f in (*.h) do (
+	move %%~nf.h include/ > nul
+)
+cd ..
 echo Generated!
